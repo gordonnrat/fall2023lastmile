dee09b9a9f1c4f8de63d25a0ea07cdf4ae2709b1
gordonnrat <gordonnrat@gmail.com>
2023-10-17 18:58:36 -0700

    inital commit

---
+++ back-end/index.ts
@@ -0,0 +1,11 @@
+import express, {Express, Request, Response} from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! from typescript')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
