6fb9a722ce54a8ea08466374ecf0b0b5a91f9c58
Rixvro <109919509+Rixvro@users.noreply.github.com>
2023-10-19 18:52:00 -0700

    trying to figure out how to import cors

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,11 +1,12 @@
-import express, {Express, Request, Response} from "express"
-const app = express()
-const port = 3000
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! from typescript')
-})
+app.route("/test").get((req, res) => {
+  res.status(200).send("Hello World! from typescript");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
