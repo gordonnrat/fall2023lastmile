c6a4016a1c2f209218e15eca8fd2db6517ae081c
Rixvro <109919509+Rixvro@users.noreply.github.com>
2023-10-24 17:09:24 -0700

    completed some basic set up for login and signup

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -4,10 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World! from typescript');
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+const port = 4000;
+app.route("/login").get((req, res) => {
+    res.status(200).send("This confirms that the login works");
+});
+app.route("/signup").post((req, res) => {
+    const data = req.body;
+    console.log(data.username);
+    console.log(data.password);
+    res.status(200).send();
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- back-end/index.ts
+++ back-end/index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from "express";
-import cors from "cors";
+import cors from 'cors';
 const app = express();
-const port = 3000;
+app.use(cors());
+app.use(express.json());
+const port = 4000;
 
-app.route("/test").get((req, res) => {
-  res.status(200).send("Hello World! from typescript");
+app.route("/login").get((req, res) => {
+  res.status(200).send("This confirms that the login works");
+});
+
+app.route("/signup").post((req, res) => {
+  const data = req.body;
+
+  console.log(data.username);
+  console.log(data.password);
+
+  res.status(200).send();
 });
 
 app.listen(port, () => {
